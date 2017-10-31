bd5ae356a85ac77491ec35fbf4134f202e319785
lixuwu <shujuku0802lixuwu@126.com>
2017-10-31 11:19:57 +0800

    test

---
+++ joice-cache/src/main/java/TestMain.java
@@ -0,0 +1,13 @@
+/** 
+ * @author lxw 
+ * @describe
+ * @date 2017年10月31日 上午11:18:26 
+ */
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
